496d173acd8295a2680c0676031ac63371024018
Krish Mittal <krishmittal212005@gmail.com>
2024-03-21 15:05:16 +0530

    Initial Commit

---
+++ AiryAI/AiryAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiryAIApp.swift
+//  AiryAI
+//
+//  Created by Krish Mittal on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AiryAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
