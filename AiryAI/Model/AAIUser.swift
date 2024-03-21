8ea8eb436c0d44d3f2e19057c82a3e68316160bc
Krish Mittal <krishmittal212005@gmail.com>
2024-03-21 17:49:11 +0530

    file system

---
+++ AiryAI/Model/AAIUser.swift
@@ -0,0 +1,8 @@
+//
+//  AAIUser.swift
+//  AiryAI
+//
+//  Created by Krish Mittal on 21/03/24.
+//
+
+import Foundation
---
+++ AiryAI/ViewModel/AuthenticationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationViewModel.swift
+//  AiryAI
+//
+//  Created by Krish Mittal on 21/03/24.
+//
+
+import Foundation
