f92114e1e80e62dfd955737126909bc279ccc245
Krish Mittal <krishmittal212005@gmail.com>
2024-03-22 09:09:49 +0530

    navigationBar

---
+++ AiryAI/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  AiryAI
+//
+//  Created by Krish Mittal on 22/03/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
