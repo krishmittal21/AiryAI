330b77de45f159f13361aa172ae696120a697d13
Krish Mittal <krishmittal212005@gmail.com>
2024-03-22 08:45:30 +0530

    tab view

--- AiryAI/MainView.swift
+++ AiryAI/MainView.swift
@@ -13,16 +13,27 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            VStack {
-                Button {
-                    viewModel.signOut()
-                } label : {
-                    Text("signout")
-                }
-            }
-            .padding()
+            accountView
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        NavigationView {
+            TabView {
+                Group {
+                    ChatView()
+                        .tabItem {Label("Chat", systemImage: "message")}
+                    AssistantView()
+                        .tabItem {Label("Assistant", systemImage: "square.grid.2x2.fill")}
+                    ChatHistoryView()
+                        .tabItem {Label("History", systemImage: "clock")}
+                }
+                .toolbarBackground(Color.backgroundColor, for: .tabBar)
+                .toolbarBackground(.visible, for: .tabBar)
+            }
         }
     }
 }
